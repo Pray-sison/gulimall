f022fc759ade2487d08ca0d7feb9d692b25c54a6
Pray-sison <liangxinfirstone@gmain.com>
2023-07-06 23:57:09 +0800

    除第三方应用所有项目均可启动

--- gulimall-order/src/main/java/io/niceseason/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/io/niceseason/gulimall/order/GulimallOrderApplication.java
@@ -2,6 +2,7 @@ package io.niceseason.gulimall.order;
 
 import com.alibaba.cloud.seata.GlobalTransactionAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
